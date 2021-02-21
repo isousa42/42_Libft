3de8a8ae5a09c7566d969dd1bdcf33e6ec7bde8a
SeniAlex <seni.alexandra98@gmail.com>
2021-02-21 14:36:26 +0000

    changes lstmap

--- Level 0/libft/ft_lstmap.c
+++ Level 0/libft/ft_lstmap.c
@@ -17,15 +17,17 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new;
 	t_list	*first;
 
-	if (!lst)
-		return ;
-	new = f(lst);
-	lst = lst->next;
-	first = new;
+	first = NULL;
+	if (!f || !del)
+		return (0);
 	while (lst)
 	{
-		new->next = f(lst);
-		new = new->next;
+		if (!(new = ft_lstnew(f(lst->content))))
+		{
+			ft_lstclear(&first, del);
+			return (0);
+		}
+		ft_lstadd_back(&first, new)
 		lst = lst->next;
 	}
 	return (first);
